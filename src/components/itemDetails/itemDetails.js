b17cf69d60344092784190cef4cb8727cc29b9ee
Abylai <you@example.com>
2021-12-18 19:46:23 +0500

    first commit

---
+++ src/components/itemDetails/itemDetails.js
@@ -0,0 +1,111 @@
+import React, {Component} from 'react';
+import './itemDetails.css';
+import ErrorMessage from '../errorMessage';
+import Spinner from '../spinner';
+
+const Field = ({item,field,label}) => {
+    return (
+        <li className='list-group-item d-flex justify-content-between'>
+            <span className='term'>{label}</span>
+            <span>{item[field]}</span>
+        </li>
+    )
+}
+
+export {
+    Field
+};
+
+export default class ItemDetails extends Component {
+
+    state = {
+        item:null,
+        loading:true,
+        error:false
+    }
+
+    componentDidMount() {
+        this.updateItem();
+    }
+
+    componentDidUpdate(prevProps) {
+        if(this.props.itemId !== prevProps.itemId) {
+            this.updateItem();
+        }
+    }
+
+    onItemDetailsLoaded = (item) => {
+        this.setState({
+            item,
+            loading:false
+        })
+    }
+
+    updateItem() {
+        const {itemId, getData} = this.props;
+        if(!itemId) {
+            return;
+        }
+    
+        this.setState({
+            loading:true
+        })
+
+        getData(itemId)
+            .then(this.onItemDetailsLoaded)
+            .catch( () => this.onError() )
+    }
+
+    onError = () => {
+        this.setState({
+            item:null,
+            error:true
+        })
+    }
+
+    showItem(item) {
+        const {name} = item;
+        return (
+            <>
+            <h4>{name}</h4>
+            <ul className='list-group list-group-flush'>
+                {
+                    React.Children.map(this.props.children,(child) => {
+                        return React.cloneElement(child,{item})
+                    })
+                }
+                </ul>
+            </>
+        )
+    }
+
+    render() {
+
+        const {item,loading,error} = this.state;
+        const errorMesssage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? 
+            <div className='item-details rounded'>
+                <Spinner/>
+            </div> : null;
+        const content = !(loading || error) ? this.showItem(item): null;
+
+        if(!item) {
+            return (
+                <div className='item-details rounded'>
+                    <span className='select-error'>Please select a character</span>
+                </div>
+            ); 
+        } else {
+            return (
+                <div className='item-details rounded'>
+                    {errorMesssage}
+                    {spinner}
+                    {content}
+                </div>
+            );
+        }
+    }
+}
+   
+
+ 
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-ripple-api2nd2g9du"><div class="ldio-kvg8xtg4j">
+        <div></div><div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
